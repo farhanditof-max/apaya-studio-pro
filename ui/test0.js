6fc4f7d34d4f1d607c79d37ababa631fb5146e52
Farhandit <farhanditof@gmail.com>
2026-06-01 11:58:36 +0700

    initial commit

---
+++ ui/test0.js
@@ -0,0 +1,1 @@
+document.addEventListener('keydown',function(e){if(e.key==='F12'||(e.ctrlKey&&e.shiftKey&&(e.key==='I'||e.key==='i'))||(e.ctrlKey&&(e.key==='U'||e.key==='u'))){e.preventDefault();return false;}});
