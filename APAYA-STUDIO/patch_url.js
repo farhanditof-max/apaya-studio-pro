b6f160f271c3cbb2dff04a62ce54db9a83f21481
Fariz Maulana <muhammad.fariz.m@gmail.com>
2026-06-10 16:11:06 +0700

    chore: remove one-off patch and debug scripts
    35 dev-time scripts (fix_*, inject_*, patch_*, recover*, etc.) no longer
    needed after god-module-split refactor. Logic absorbed into lib/ modules.

--- APAYA-STUDIO/patch_url.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const targetFile = 'K:\\Project\\## PLUGINS APAYA\\APAYA-STUDIO\\main.rb';
-let content = fs.readFileSync(targetFile, 'utf8');
-
-// Replace all .gsub('\\', '/') or .gsub("\\", "/") with .gsub('\\', '/').gsub(' ', '%20').gsub('#', '%23')
-content = content.replace(/\.gsub\(['"]\\\\['"], ['"]\/['"]\)/g, ".gsub('\\\\', '/').gsub(' ', '%20').gsub('#', '%23')");
-
-fs.writeFileSync(targetFile, content, 'utf8');
-console.log("main.rb URL encoding patched!");
